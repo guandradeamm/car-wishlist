45078e8ff7a25fb60516f82995435958a6e0c171
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2021-10-06 15:43:31 -0300

    feat: add display images

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { SliderData } from "./SliderData";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
-const ImageSlider = () => {
-  return <div>Images</div>;
+const ImageSlider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  return (
+    <section className="slider">
+      <FaArrowAltCircleLeft className="left-arrow" />
+      <FaArrowAltCircleRight className="right-arrow" />
+      {SliderData.map((slide, index) => {
+        return <img src={slide.image} alt="travel image" />;
+      })}
+    </section>
+  );
 };
 
 export default ImageSlider;
